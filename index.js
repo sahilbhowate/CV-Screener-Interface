5c283a758c47c65f8a06e432cd61a51343e264ba
Sahil <idcpowai@gmail.com>
2021-09-15 09:28:54 +0530

    CV Screener Project

---
+++ index.js
@@ -0,0 +1,96 @@
+console.log("This is my index.js");
+
+// Data is an array of objects which contains information about the candidates
+
+const data = [
+  {
+    naam: 'Rohan Das',
+    age: 18,
+    city: 'Kolkata',
+    language: 'Python',
+    framework: 'Django',
+    image: 'https://randomuser.me/api/portraits/men/75.jpg'
+  },
+
+  {
+    naam: 'Shubham Sharma',
+    age: 28,
+    city: 'Banglore',
+    language: 'JavaScript',
+    framework: 'Angular',
+    image: 'https://randomuser.me/api/portraits/men/76.jpg'
+  },
+
+  {
+    naam: 'Camelia Cabello',
+    age: 18,
+    city: 'Kolkata',
+    language: 'Python',
+    framework: 'Django',
+    image: 'https://randomuser.me/api/portraits/women/77.jpg'
+  },
+
+  {
+    naam: 'Aishwarya Rai',
+    age: 42,
+    city: 'Mumbai',
+    language: 'Python',
+    framework: 'Flask',
+    image: 'https://randomuser.me/api/portraits/women/78.jpg'
+  },
+
+  {
+    naam: 'Rohit Sharma',
+    age: 16,
+    city: 'Jharkhand',
+    language: 'Go',
+    framework: 'Go Framework',
+    image: 'https://randomuser.me/api/portraits/men/79.jpg'
+  }
+
+]
+
+//CV Iterator
+function cvIterator(profiles) {
+  let nextIndex = 0;
+  return {
+    next: function () {
+      return nextIndex < profiles.length ?
+        { value: profiles[nextIndex++], done: false } :
+        { done: true }
+    }
+  };
+}
+const candidates = cvIterator(data);
+
+nextCV();
+
+//Button listener for next button
+const next = document.getElementById('next');
+
+next.addEventListener('click', nextCV);
+
+
+function nextCV() {
+  const currentCandidate = candidates.next().value;
+
+  let image = document.getElementById('image');
+  let profile = document.getElementById('profile');
+
+  if (currentCandidate != undefined) {
+    image.innerHTML = `<img src='${currentCandidate.image}'>`;
+
+    profile.innerHTML = `<ul class="list-group">
+  <li class="list-group-item">Name: ${currentCandidate.naam}</li>
+  <li class="list-group-item">${currentCandidate.age} years old</li>
+  <li class="list-group-item">Lives in ${currentCandidate.city}</li>
+  <li class="list-group-item">Primarily works on ${currentCandidate.language}</li>
+  <li class="list-group-item">Uses ${currentCandidate.framework} framework</li>
+</ul>`
+  }
+
+  else {
+    alert('End of candidate applications');
+    window.location.reload();
+  }
+}
